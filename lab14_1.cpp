f0dc8d0c221b31144160ac3db44f8d83967ffe11
SupasanRuang <supasan_ruang@cmu.ac.th>
2021-12-28 15:11:02 +0700

    lab14_1.cpp completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,37 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double data[],int N ,double ans[])
+{
+    double sumA=0 ,sumS =0,multG=1,sumH=0,max=data[0],min=data[0],num=N;
+    for (int i = 0; i < num; i++)
+    {
+        sumA +=data[i];
+        sumS+=(data[i]*data[i]);
+        multG*=data[i];
+        sumH+=(1/data[i]);
+        if(data[i]>max)
+        {
+            max=data[i];
+        }
+        if(data[i]<min)
+        {
+            min=data[i];
+        }
+
+        
+    }
+    
+    ans[0]=sumA/num;
+    ans[1]=sqrt(((sumS/num)-pow(ans[0],2)));
+    ans[2]=pow(multG,(1/num));
+    ans[3]=num/sumH;
+    ans[4]=max;
+    ans[5]=min;
+
+
+
+
+
+}
